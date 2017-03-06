c885b7921c4df301c0a13cdc6be3531a63be88b5
Raarup <rraa@itu.dk>
2017-03-06 10:58:12 +0100

    hej drenge

--- src/bfst17/DrawWindow.java
+++ src/bfst17/DrawWindow.java
@@ -2,6 +2,9 @@ package bfst17;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Observable;
 import java.util.Observer;
@@ -23,6 +26,7 @@ public class DrawWindow implements Observer {
 		canvas.setPreferredSize(new Dimension(500, 500));
 		new CanvasMouseController(canvas, model);
 		window.add(canvas, BorderLayout.CENTER);
+		setUpMenu();
 		window.pack();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
@@ -30,6 +34,8 @@ public class DrawWindow implements Observer {
 		canvas.zoom(window.getWidth() / (model.getMaxLon() - model.getMinLon()));
 		new WindowKeyController(this, model);
 	}
+
+
 
 	/**
 	 * This method is called whenever the observed object is changed. An
@@ -42,7 +48,6 @@ public class DrawWindow implements Observer {
 	 */
 	@Override
 	public void update(Observable o, Object arg) {
-//hej
 	}
 
 	public void addKeyListener(KeyListener keyListener) {
@@ -52,4 +57,70 @@ public class DrawWindow implements Observer {
 	public void toggleAA() {
 		canvas.toggleAA();
 	}
+
+
+	public void setUpMenu(){
+		JMenuBar menu = new JMenuBar();
+		JMenu file = new JMenu("File");
+		JMenuItem save = new JMenuItem("Save", KeyEvent.VK_S);
+		save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, 0));
+		JMenuItem load = new JMenuItem("Load", KeyEvent.VK_L);
+		load.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, 0));
+		JMenuItem exit = new JMenuItem( "Exit", KeyEvent.VK_Q);
+
+
+		file.add(save);
+		file.add(load);
+		file.add(exit);
+		menu.add(file);
+
+		JMenu tools = new JMenu("Tools");
+		JMenuItem pan = new JMenuItem("Pan", KeyEvent.VK_P);
+		pan.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, 0));
+
+		tools.add(pan);
+		menu.add(tools);
+
+		window.setJMenuBar(menu);
+
+
+		//metode til at save
+		save.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String savename = JOptionPane.showInputDialog("Enter a filename to save!");
+				savename += ".bin";
+				model.save(savename);
+			}
+		});
+
+		//metode til at loade
+		load.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String loadname = JOptionPane.showInputDialog("Enter the name of the file you want to load.");
+				loadname += ".bin";
+				model.load(loadname);
+			}
+		});
+
+		exit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+
+
+		pan.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				CanvasMouseController.isPanning();
+			}
+		});
+
+
+	}
 }
+
