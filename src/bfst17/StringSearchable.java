ec259d52d50e8fa0e18e809681ee2a449b09eda7
Jakob Roos <jroo@itu.dk>
2017-03-08 13:09:29 +0100

    Added priority to autocompleter

--- src/bfst17/StringSearchable.java
+++ src/bfst17/StringSearchable.java
@@ -1,13 +1,10 @@
 package bfst17;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class StringSearchable {
     private List<String> databaseInfo = new ArrayList();
-
+    PriorityQueue<PriorityStrings> priority = new PriorityQueue<>();
     public StringSearchable(List<String> databaseInfo) {
         this.databaseInfo.addAll(databaseInfo);
     }
@@ -15,13 +12,19 @@ public class StringSearchable {
     public Collection<String> search(String userInput) {
         ArrayList founds = new ArrayList();
         Iterator var3 = this.databaseInfo.iterator();
-
+        double max = 0;
         while(var3.hasNext()) {
             String s = (String)var3.next();
             if(s.indexOf(userInput) == 0 || s.contains(userInput)) {
-                founds.add(s);
+                double n = ((double) userInput.length()/(double) s.length());
+                priority.add(new PriorityStrings(n,s));
+                }
+
             }
-        }
+            for(int i = 0; i <10;i++){
+                founds.add(priority.poll().getAddress());
+            }
+
 
         return founds;
     }
