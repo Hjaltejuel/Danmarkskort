e0bb54c10e58666022035231aecedc6dd6c20290
Hjaltejuel <hjsk@itu.dk>
2017-03-13 14:28:10 +0100

    Nyeste version

--- src/bfst17/CanvasMouseController.java
+++ src/bfst17/CanvasMouseController.java
@@ -74,12 +74,15 @@ public class CanvasMouseController extends MouseAdapter {
 		Point2D currentMousePosition = e.getPoint();
 		double dx = currentMousePosition.getX();
 		double dy = currentMousePosition.getY();
+		canvas.setCenter(-dx/canvas.getXZoomFactor(),-dy/canvas.getYZoomFactor());
 		canvas.pan(-dx,-dy);
-		double oldDx = (((canvas.getWidth()/2)-dx)/canvas.getXZoomFactor());
-		double oldDy = (((canvas.getHeight()/2)-dy)/canvas.getYZoomFactor());
 		canvas.zoom(factor);
+		canvas.setCenter(dx/canvas.getXZoomFactor(),dy/canvas.getYZoomFactor());
 		canvas.pan(dx,dy);
-		canvas.setCenter(oldDx*0.1111111,oldDy*0.11111111);
+
+
+
+
 
 	}
 }
