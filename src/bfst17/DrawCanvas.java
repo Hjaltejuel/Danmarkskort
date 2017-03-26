4940e78c09b9c89e32d8fd514ec0ae10e3462f57
Hjaltejuel <hjsk@itu.dk>
2017-03-27 00:00:47 +0200

    Pointer fixed

--- src/bfst17/DrawCanvas.java
+++ src/bfst17/DrawCanvas.java
@@ -171,7 +171,6 @@ public class DrawCanvas extends JComponent implements Observer {
             double offsetWidth = ((image.getWidth()/4)/transform.getScaleX())/7;
             imageTransform.translate(-pin.getX(),-pin.getY());
             imageTransform.scale(((1/transform.getScaleX())/7),((1/transform.getScaleY())/7));
-            System.out.println("hello");
             ((Graphics2D) g).drawImage(image, imageTransform, null);
             searchMode = false;
         }
